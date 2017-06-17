90f486e94bc9a7a9cc2e5ab78ac1ca1d206d69fe
bottyrblx <bottyrblx@Outlook.com>
2017-06-17 17:54:29 +0200

    u

--- commands/downtime.js
+++ commands/downtime.js
@@ -1,19 +1,22 @@
 const fs = require('fs')
 exports.run = function(bot, message, args){
  let main = require('../maintainance.json')
+ let string = JSON.stringify('.//')
  if(main.status === true) {
      var maint = {
          status: false,
          reason: ""
      }
-     fs.writeFileSync('../maintainance.json', maint, 'utf8')
+     let dataa = JSON.stringify(maint)
+     fs.writeFile('../maintainance.json', dataa, 'utf8')
  } else {
      if(args[0]){
          var maintt = {
              status: true,
              reason: args.join(' ')
          }
-         fs.writeFileSync('../maintainance.json', maintt, 'utf8')
+         let boob = JSON.stringify(maintt)
+         fs.writeFile('../maintainance.json', boob, 'utf8')
          message.channel.send('Successfully enabled downtime for reason: ' + args.join(' '))
      } else {
          message.channel.send('Please specify a reason.')
